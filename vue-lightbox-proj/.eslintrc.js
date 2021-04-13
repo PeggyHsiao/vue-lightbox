008f24e2d75017e6b4fd9b744e8e50defd6e4369
Peggy.Hsiao <Peggy.Hsiao@advantech.com.tw>
2021-04-13 10:26:59 +0800

    [feature] basic setting (eslint, settings.json)

---
+++ vue-lightbox-proj/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended ',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "linebreak-style": [
+      error,
+      windows
+    ],
+    "import/extensions": [
+      error,
+      always,
+      { js: "never", vue: "never", json: "never" }
+    ]
+  },
+};
